ddfb563e6764c19b8309a1dcad92f0929b8f1ff1
Alejandra Gomez <alejita.9813.dg@gmail.com>
2018-08-28 15:39:28 -0500

    primera parte lab

--- src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/GrammarChecker.java
@@ -3,9 +3,10 @@ package edu.eci.arsw.springdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class GrammarChecker {
-
+        
+        @Autowired
 	SpellChecker sc;
 
 	String x;
@@ -14,7 +15,8 @@ public class GrammarChecker {
 	public SpellChecker getSpellChecker() {
 		return sc;
 	}
-
+        
+        @Autowired
 	public void setSpellChecker(SpellChecker sc) {
 		this.sc = sc;
 	}
